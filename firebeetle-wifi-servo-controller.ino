b4ae8611f6ab9490060c5bbd41978069be9fe3ec
Stu Wares <stuwares@outlook.com>
2022-09-05 18:52:12 +0100

    Added routing for on and off

--- firebeetle-wifi-servo-controller.ino
+++ firebeetle-wifi-servo-controller.ino
@@ -11,8 +11,11 @@ void setup() {
 
   Serial.begin(115200);
 
+  pinMode(LED_BUILTIN, OUTPUT);
+
   WiFi.begin(ssid, password);
 
+  // Wait until WiFi connects
   while (WiFi.status() != WL_CONNECTED ) {
     delay(1000);
     Serial.println("Connecting to wifi...");
@@ -20,9 +23,26 @@ void setup() {
 
   Serial.println("Wifi comnected, IP address:");
   Serial.println(WiFi.localIP());
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/html", "<a href='/h'><button>Switch light on</button><a><br /><a href='/l'><button>Switch light off</button><a>");
+  });
+
+  server.on("/h", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", "Light is on");
+    digitalWrite(LED_BUILTIN, HIGH);
+  });
+
+  server.on("/l", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", "Light is off");
+    digitalWrite(LED_BUILTIN, LOW);
+  });
+
+  server.begin();
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
 }
